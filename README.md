075c58eff5471d34be545b16653dd983bb0c7918
Faust Celaj <faustcelaj@fausts-mbp.home>
2024-02-06 19:57:10 -0500

    updated readme

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - [Description](#description)
 - [Installation](#installation)
 - [Usage](#usage)
+- [Screenshots](#Screenshots)
 
 ## Description
 A simple online text editor that is also a PWA (progressive web application).
@@ -14,3 +15,5 @@ You can click the install button.
 
 ## Usage
 When typing in the text editor it will automatically save and be available for future use
+
+## Screenshots
---
+++ client/src/js/database.js
@@ -0,0 +1,43 @@
+import { openDB } from "idb";
+let database;
+
+const initdb = async () =>
+  database ||
+  (database = openDB("jate", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  }));
+
+export const putDb = async (content) => {
+  try {
+    const db = await openDB("jate", 1);
+    const tx = db.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    const result = await store.put({ content: content, id: 1 });
+    console.log("Data added to the database", result);
+  } catch (error) {
+    console.error("putDb not implemented", error);
+  }
+};
+
+export const getDb = async () => {
+  try {
+    console.log("Get all from the database.");
+    const db = await openDB("jate", 1);
+    const tx = db.transaction("jate", "readonly");
+    const store = tx.objectStore("jate");
+    const result = await store.get(1);
+    console.log("Data retrieved from the database.", result);
+    return result?.value;
+  } catch (error) {
+    console.error("Error retrieving data from the database", error);
+  }
+};
+
+initdb();
